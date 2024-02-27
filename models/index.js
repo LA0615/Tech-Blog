ebffc2744904631eb10e1821eca75b12e39c7372
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2024-02-26 22:54:52 -0500

    one to many index.js file

--- models/index.js
+++ models/index.js
@@ -2,12 +2,28 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// Creates a relationship between USER and post model
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
+// Creates a relationship between USER and comment model
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
+// Creates a relationship between the post and the user
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
-
-
-
-
+// In the Comment model
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = { User, Post, Comment };
